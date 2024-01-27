7fa64547c9fb3026fc82beef6bee1b67b5000072
danielachaburgil <danielachaburgilvergara@gmail.com>
2024-01-27 17:26:45 -0500

    unidad java

---
+++ Unidad JavaScript/index.js
@@ -0,0 +1,13 @@
+// volumen = ancho x largo x alto
+// Volumen = 16 cm x 23 cm x 13 cm
+
+const volumen = 16 * 23 * 13;
+console.log('volumen', volumen + ' cm3');
+
+//Area = 2(a*b) + 2(b*c) + 2(a*c)
+//Area = 2 (16*23) + 2(13*23) + 2(16*23)
+const area1 = 2*(16*23);
+const area2 = 2*(13*23);
+const area3 = 2*(16*23);
+const areatotal = area1 + area2 + area3
+console.log('areatotal', areatotal + ' cm2');
